45078beb91dc36c6f45eaf42687da012d1ff5979
Ari-Pekka Repo <aprepo@gmail.com>
2024-10-12 18:54:02 +0300

    Some variables stuff

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Hello world")
+
+	fmt.Println("Try out different variables and stuff")
+	var a = "a"
+	fmt.Println("a: " + a)
+
+	var b bool = true
+	fmt.Println("b: " + strconv.FormatBool(b))
+
+	var i int = 1
+	fmt.Println("i: " + strconv.Itoa(i))
+
+	j := 2 // No var needed if using :=
+	fmt.Println("j: " + strconv.Itoa(j))
+
+	const c int = 10
+	fmt.Println("c: " + strconv.Itoa(c) + " is a constant and can't be modified")
 }
